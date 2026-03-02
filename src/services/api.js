ff91c9fb0afc4e2601b9703c8fed426010909077
Nguyen Duy Khanh - SE183076 <khanhndse183076@fpt.edu.vn>
2026-03-02 10:12:24 +0700

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: BASE_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Attach JWT token to every request if available
